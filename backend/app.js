990dbd83685427b6eafc8dc638b3cb0ba07270f1
Benny Luo <bennyluo95@gmail.com>
2021-10-05 23:21:50 -0400

    set up express server

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const config = require('./utils/config')
+const logger = require('./utils/logger')
 const express = require('express')
 const app = express()
 require('express-async-errors')
@@ -7,10 +8,19 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const resultsRouter = require('./controllers/results')
 
+//mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
 app.use(cors())
 app.use(express.json())
 
-
+app.use('/api/results', resultsRouter)
 
 
 module.exports = app
--- backend/controllers/results.js
+++ backend/controllers/results.js
@@ -4,16 +4,17 @@ const Results = require('../models/results')
 
 router.get('/', async (request, response) => {
     const results = await Results
-      .find({}).populate('result', { date: 1, winner: 1 })
+      .find({})
+
     response.json(results)
   })
 
-router.post('/api/results/', async (request, response) => {
-  const resultInput = new Result(request.body)
+router.post('/', async (request, response) => {
+  const resultInput = new Results(request.body)
 
   const savedResults = await resultInput.save()
 
-  response.status(201).json(resultInput)
+  response.status(201).json(savedResults)
 })
 
 module.exports = router
